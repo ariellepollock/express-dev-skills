d7069f3c4747090d5362fe8846e93e0e93690150
ariellepollock <ariellecpollock@gmail.com>
2023-12-12 19:06:41 -0600

    add skills array

---
+++ models/skills.js
@@ -0,0 +1,11 @@
+const skills = [
+    {id: 1, skill: 'Photoshop', years: 10},
+    {id: 2, skill: 'Illustrator', years: 14},
+    {id: 3, skill: 'XD', years: 6},
+    {id: 4, skill: 'Lightroom', years: 12},
+    {id: 5, skill: 'Rhino', years: 10},
+    {id: 6, skill: 'Premier Pro', years: 2},
+    {id: 7, skill: 'InDesign', years: 4},
+    {id: 8, skill: 'Midjourney', years: 1},
+    {id: 9, skill: 'InCopy', years: 1}
+]
