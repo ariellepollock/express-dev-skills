a2908b1c682ed5f859a7c0d34179ad84048a6cc7
ariellepollock <ariellecpollock@gmail.com>
2023-12-13 13:07:10 -0600

    add partials

--- models/skill.js
+++ models/skill.js
@@ -4,15 +4,15 @@ module.exports = {
 }
 
 const skills = [
-    {id: 1, skill: 'Photoshop', years: 10},
-    {id: 2, skill: 'Illustrator', years: 14},
-    {id: 3, skill: 'XD', years: 6},
-    {id: 4, skill: 'Lightroom', years: 12},
-    {id: 5, skill: 'Rhino', years: 10},
-    {id: 6, skill: 'Premier Pro', years: 2},
-    {id: 7, skill: 'InDesign', years: 4},
-    {id: 8, skill: 'Midjourney', years: 1},
-    {id: 9, skill: 'InCopy', years: 1}
+    {id: 1, skill: 'Photoshop'},
+    {id: 2, skill: 'Illustrator'},
+    {id: 3, skill: 'XD'},
+    {id: 4, skill: 'Lightroom'},
+    {id: 5, skill: 'Rhino'},
+    {id: 6, skill: 'Premier Pro'},
+    {id: 7, skill: 'InDesign'},
+    {id: 8, skill: 'Midjourney'},
+    {id: 9, skill: 'InCopy'}
 ]
 
 function getOne(id) {
